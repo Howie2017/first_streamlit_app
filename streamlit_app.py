6e2d09666530a6fcf67ad86bf65d6f32db06aad1
Howard White <howie2447@yahoo.co.uk>
2022-11-02 02:07:56 +0000

    Update streamlit_app.py
    Error in code as emoji accidentally left on script page

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,3 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
-
-
-  🍞
