fceb2850dc4740a3b111ed98fd695fa7924852c3
Howard White <howie2447@yahoo.co.uk>
2022-11-16 20:42:07 +0000

    Update streamlit_app.py
    Response

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
-#streamlit.text(fruityvice_response.json()) # juat writes the data to the screen
+streamlit.text(fruityvice_response.json()) # juat writes the data to the screen
 
 
 # take the json version of the response and normalise it
