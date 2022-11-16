4e6b86e304d6c1854552c190d010b07a4c26dbec
Howard White <howie2447@yahoo.co.uk>
2022-11-16 20:24:47 +0000

    Update streamlit_app.py
    Response edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+#New section to display frutiyvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json()) # juat writes the data to the screen
 
 # take the json version of the response and normalise it
