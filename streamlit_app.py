bae4da5241ffa502af2b8516393fc5335be32211
Howard White <howie2447@yahoo.co.uk>
2022-11-18 18:14:52 +0000

    Update streamlit_app.py
    Data rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cur.execute("use warehouse pc_rivery_wh")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchone()
 streamlit.header("The Fruit List Contains:")
