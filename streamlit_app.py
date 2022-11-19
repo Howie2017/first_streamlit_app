0bea31791795cbf1085f5d53ea73943bb9fdcb0d
Howard White <howie2447@yahoo.co.uk>
2022-11-19 14:18:09 +0000

    Update streamlit_app.py
    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,8 @@ streamlit.write('The user entered ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add', 'Cantaloupe')
-streamlit.write('The user entered ', add_my_fruit)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
 #This will not work coorrectly but just go with it for now
