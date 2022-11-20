911bcdf6e251bc5907d74e8a19aa1a33299e64da
Howard White <howie2447@yahoo.co.uk>
2022-11-20 23:59:43 +0000

    Update streamlit_app.py
    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header('Fruityvice Fruit Advice')
+#streamlit.header('Fruityvice Fruit Advice')
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -53,6 +53,23 @@ try:
 except URLError as e:
 	streamlit.error()
 	
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return	fruityvice_normalized
+
+#New Section to display fruityvice api response	
+streamlit.header('Fruityvice Fruit Advice!')	
+try:
+	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")	
+else:
+	back_from_function = get_fruityvice_data( fruit_choice
+	streamlit.dataframe( back_from_function)
+					 
+					 
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
